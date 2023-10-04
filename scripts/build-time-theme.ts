f8eca6d74eef269549f8cba09e9dd1bd6eff3c19
Vladimir Skok <vladsk@amazon.com>
2023-10-03 22:29:04 -0400

    change build-time-theme script theme

--- scripts/build-time-theme.ts
+++ scripts/build-time-theme.ts
@@ -1,4 +1,4 @@
 import { updateCssTheme } from '../src/theming/build-time.ts';
-import { ukConfig } from '../src/configs/uk.ts';
+import { usConfig } from '../src/configs/us.ts';
 
-updateCssTheme(ukConfig.theme);
+updateCssTheme(usConfig.theme);
