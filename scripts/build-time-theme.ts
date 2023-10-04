8eb5e75839dec2b100fdc57c779bf6fb336cd87c
Vladimir Skok <vladsk@amazon.com>
2023-10-03 22:03:20 -0400

    fix build-time-theme script imports

--- scripts/build-time-theme.ts
+++ scripts/build-time-theme.ts
@@ -1,4 +1,4 @@
-import { updateCssTheme } from '../src/theming/build-time';
-import { ukConfig } from '../src/configs/uk';
+import { updateCssTheme } from '../src/theming/build-time.ts';
+import { ukConfig } from '../src/configs/uk.ts';
 
 updateCssTheme(ukConfig.theme);
